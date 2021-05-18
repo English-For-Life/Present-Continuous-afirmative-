9d0862c0cf18898e4071c535ce189cc3dd65c103
English-For-Life <83182532+English-For-Life@users.noreply.github.com>
2021-05-18 13:25:24 +0300

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,54 @@
+//Array of objects
+const quiz = [
+    {
+        q:"Listen! He ___ the piano.",
+        options:["plays", "is playing", "are playing", "is play"],
+        answer: 1
+    },
+    {
+     q:"They ___ a new hotel in the city centre at the moment.",
+        options:["are building", "is building", "build", "building"],
+        answer: 0   
+    },
+    {
+        q:"Look! Somebody ___ in the river.",
+        options:["swim", "are swimming", "is swimming", "swimming"],
+        answer: 2
+    },
+    {
+        q:"'You ___ on my foot!'. 'Oh, I'm sorry'.",
+        options:["are stand", "standing", "stand", "are standing"],
+        answer: 3
+    },
+    {
+        q:"Hurry up! The bus ___ .",
+        options:["are coming", "coming", "is coming", "is come"],
+        answer: 2
+    },
+    {
+        q:"'Where are you?' 'In the kitchen. I ___ a meal'.",
+        options:["cooking", "am cooking", "am cook", "is cooking"],
+        answer: 1
+    },
+    {
+        q:"She ___ a shower at the moment.",
+        options:["is having", "are having", "has", "having"],
+        answer: 0
+    },
+    {
+        q:"Look! It ___ .",
+        options:["is snow", "snow", "snowing", "is snowing"],
+        answer: 3
+    },
+    {
+        q:"I am very busy. I ___ my homework.",
+        options:["am do", "'m doing", "doing", "do"],
+        answer: 1
+    },
+    {
+        q:"I need to go home. It ___ dark.",
+        options:["getting", "is get", "is getting", "get"],
+        answer: 2
+    }
+
+]
